8bb374ff5640867cd3efbd2da1fa603c8c7d731f
John Gray <100069417+anchorGray@users.noreply.github.com>
2022-09-18 20:12:46 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,37 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+import datetime
+from django.db import models
+from django.utils.timezone import now
 
+
+# Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car make')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    WAGON = 'wagon'
+    PICKUP = 'pickup'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (PICKUP, 'Pickup')
+    ]
+    YEAR_CHOICES = [(r,r) for r in range(1990, datetime.date.today().year+1)]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default='car model')
+    dealer_id = models.IntegerField()
+    _type = models.CharField(null=False, max_length=30, choices=TYPE_CHOICES)
+    year = models.DateField()
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -25,3 +55,53 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+    def __str__(self):
+        return "Name: " + self.name 
+                "Make: " + self.car_make + \
+                "Dealer ID: " + self.dealer_id + \
+                "Type: " + self._type + \
+                "Year: " + self.year
+
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.id = id
+        self.review = review
+        self.ourchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+
+    def __str__(self):
+        return "Review :" + self.review + \
+                "Name: " + self.name + \
+                "Sentiment: " + self.sentiment
