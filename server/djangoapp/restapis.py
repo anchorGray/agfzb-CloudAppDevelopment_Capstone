9bc07bb03811f87c790cc980f3cbceeba24fcedc
John Gray <100069417+anchorGray@users.noreply.github.com>
2022-09-29 11:38:38 -0700

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -119,7 +119,7 @@ def get_dealer_reviews_from_cf(url, **kwargs):
         for dealer_review in reviews:
             dealer_review = reviews
             
-            review_obj = DealerReview(dealership=dealer_review,
+            review_obj = DealerReview(dealership=dealer_review[],
                                    name=dealer_review["name"],
                                    purchase=dealer_review["purchase"],
                                    review=dealer_review["review"])
