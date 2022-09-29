98b59a7692a9333940b9c8c08b0a1879e49c0ea6
John Gray <100069417+anchorGray@users.noreply.github.com>
2022-09-28 21:36:05 -0700

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -107,7 +107,7 @@ def get_dealers_by_st_from_cf(url, state):
 
 def get_dealer_reviews_from_cf(url, **kwargs):
     results = []
-    id = kwargs['kwargs']['dealership']
+    id = kwargs['dealership']
     json_result = get_request(url, id=id)
     if json_result:
         # Get the row list in JSON as dealers
